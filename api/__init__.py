8b0d8bd636872f8af12c0945f1edba007dbd1cac
vishnu <vishnucheruvakad@gmail.com>
2021-07-17 13:47:53 +0530

    initialised a flask api

---
+++ api/__init__.py
@@ -0,0 +1,19 @@
+import os
+from flask import Flask, render_template
+
+def create_app(test_config=None):
+    app = Flask("notes_app")
+    app.config.from_mapping(
+        DATABASE=os.path.join(app.instance_path, 'notes_app.sqlite')
+    )
+    if test_config is not None:
+        app.config.update(test_config)
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+    app.config['ENV'] = 'development'
+    app.config['DEBUG'] = True
+   return app
+
+    
